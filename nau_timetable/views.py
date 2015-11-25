bebdaf24d5f921e6b2c97e0c683d56a220d2a63b
Anna Kurylo <anna.kurilo@outlook.com>
2015-11-25 17:54:51 +0200

    Make 'feature' layout

--- nau_timetable/views.py
+++ nau_timetable/views.py
@@ -1,16 +1,28 @@
 import logging
 from django.shortcuts import render
-from django.utils import timezone
-from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 
 logger = logging.getLogger(__name__)
 
+
 def timetable_landing(request):
-	logger.info(request)
-	return render(request, 'landing/promo.html')
+    logger.info(request)
+    feature_list = (
+        {'icon': 'img/glasses2.svg', 'description': ('Перегляд розкладу '
+         'групи (дізнайся, яка зараз пара)'), 'class': 'feature-block'},
+        {'icon': 'img/edit45.svg', 'description': ('Редагування розкладу '
+         '(доступне командирам груп)'), 'class': 'feature-block'},
+        {'icon': 'img/school84.svg', 'description': ('Перегляд розкладу'
+         ' викладача'), 'class': 'feature-block'},
+        {'icon': 'img/checkmark2.svg', 'description': ('Перегляд графіку '
+         'екзаменів та практики'), 'class': 'feature-block'},
+        {'icon': 'img/sun127.svg', 'description': 'Канікули',
+         'class': 'feature-block'},
+        )
+    return render(request, 'landing/promo.html',
+                  {'feature_list': feature_list})
+
 
 def timetable_about(request):
-	return render(request, 'landing/about.html')
-
+    return render(request, 'landing/about.html')
