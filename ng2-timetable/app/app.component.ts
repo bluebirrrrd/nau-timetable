af2c6b134a70ef3a0f29a238d34d125aa1defe05
Anna Kurylo <anna.kurilo@outlook.com>
2016-02-26 16:13:44 +0200

    Set up dependencies for proper work

--- ng2-timetable/app/app.component.ts
+++ ng2-timetable/app/app.component.ts
@@ -1,3 +1,20 @@
-export class AppComponent {
-    
-}
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {ScheduleComponent} from './schedule/schedule.component';
+import {RoomSearchComponent} from './room-search/room-search.component';
+
+@Component({
+  selector: 'timetable-app',
+  template: `
+    <h1>App Component</h1>
+    <router-outlet></router-outlet>
+  `,
+  directives: [ROUTER_DIRECTIVES]
+})
+
+@RouteConfig([
+  {path:'/', name: 'Schedule', component: ScheduleComponent},
+  {path:'/room-search', name: 'RoomSearch', component: RoomSearchComponent}
+])
+export class AppComponent { }
