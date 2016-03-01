17357a7242a2873a0ff3735914ca09c287778c36
Anna Kurylo <anna.kurilo@outlook.com>
2016-03-01 17:21:05 +0200

    Add ng2-material library

--- ng2-timetable/app/main.ts
+++ ng2-timetable/app/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser'
+import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from "ng2-material/all";
 import {AppComponent} from './app.component'
 
 bootstrap(AppComponent);
