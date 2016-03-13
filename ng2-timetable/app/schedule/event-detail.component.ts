e31661c656db02bd78009bfeb06481f1129f5af3
Anna Kurylo <anna.kurilo@outlook.com>
2016-03-13 13:24:59 +0200

    Create EventDetailComponent and basic template

--- ng2-timetable/app/schedule/event-detail.component.ts
+++ ng2-timetable/app/schedule/event-detail.component.ts
@@ -1,8 +1,35 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+
+import {EventsService} from './events.service';
+import {Event} from './event';
 
 @Component({
     selector: 'event-detail',
-    template: 'Detail works'
+    templateUrl: 'static/templates/event-detail.html',
+    providers: [EventsService]
 })
 
-export class EventDetailComponent { }
+export class EventDetailComponent implements OnInit {
+    constructor(
+        private _eventsService: EventsService,
+        private _routeParams: RouteParams) {}
+
+    errorMessage: string;
+    event: Event;
+
+    ngOnInit() {
+        let id = +this._routeParams.get('id');
+        this.getEvent(id);
+    }
+
+    getEvent(id: number) {
+        return this._eventsService.getEvent(id)
+            .then(
+                event => {
+                    console.log(event);
+                    this.event = event;
+                },
+                error => this.errorMessage = <string>error);
+    }
+ }
