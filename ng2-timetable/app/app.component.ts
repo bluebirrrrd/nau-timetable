5e6a410358785f008f708026d0964fe81b047892
Anna Kurylo <anna.kurilo@outlook.com>
2016-03-13 13:26:11 +0200

    Add ng2-material directives to AppComponent

--- ng2-timetable/app/app.component.ts
+++ ng2-timetable/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {HTTP_PROVIDERS}    from 'angular2/http';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 import {ScheduleComponent} from './schedule/schedule.component';
 import {RoomSearchComponent} from './room-search/room-search.component';
 import {RegisterComponent} from './register/register.component';
@@ -16,7 +17,10 @@ import {RegisterComponent} from './register/register.component';
     </nav>
     <router-outlet></router-outlet>
   `,
-  directives: [ROUTER_DIRECTIVES],
+  directives: [
+    ROUTER_DIRECTIVES,
+    MATERIAL_DIRECTIVES
+  ],
   providers: [
     ROUTER_PROVIDERS,
     HTTP_PROVIDERS
