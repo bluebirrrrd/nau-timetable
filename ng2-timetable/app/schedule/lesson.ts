5c9255d881738fee004e8b83aa14fc24a1cf0931
Anna Kurylo <anna.kurilo@outlook.com>
2016-04-25 14:27:09 +0300

    Create lesson class

---
+++ ng2-timetable/app/schedule/lesson.ts
@@ -0,0 +1,21 @@
+export class Lesson {
+    url: string;
+    number: number;
+    number_text: string;
+    day: number;
+    day_text: string;
+    week: boolean;
+    week_text: string;
+    type: number;
+    type_text: string;
+    subject: string;
+    subject_name: string;
+    teacher: string;
+    teacher_short_name: string;
+    groups: string[];
+    groups_names: string[];
+    subgroup_num: any;
+    subgroup_name: any;
+    room: string;
+    room_full_name: string;
+}
