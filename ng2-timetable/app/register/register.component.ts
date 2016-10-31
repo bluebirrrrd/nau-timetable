a6fc371100a9c7448cfcb7be6831e01796838a96
Anna Kurylo <anna.kurilo21@gmail.com>
2016-10-31 18:35:18 +0200

    Add regisration component

--- ng2-timetable/app/register/register.component.ts
+++ ng2-timetable/app/register/register.component.ts
@@ -2,19 +2,19 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {StudentRegformComponent} from './student-regform.component';
+import {RegistrationComponent} from './registration.component';
 
 @Component({
   selector: 'register',
   template: `
-    <h1>Register</h1>
-    <a [routerLink]="['RegisterStudent']">Я студент</a>
-    <a href="#">Я викладач</a>
+    <h1 style="text-align: center">Реєстрація</h1>
+    <regForm></regForm>
     <router-outlet></router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES]
 })
 
 @RouteConfig([
-  {path:'/', name: 'RegisterStudent', component: StudentRegformComponent, useAsDefault: true}
+  {path:'/', name: 'Reg', component: RegistrationComponent, useAsDefault: true}
 ])
 export class RegisterComponent { }
